7846dd8b51e48760934997d285b68451992ea974
Pedro Augusto Barbosa Aparecido <pedroaugustobarosa@inatel.br>
2023-11-22 10:45:41 -0300

    caskjbc

--- index.js
+++ index.js
@@ -40,9 +40,9 @@ app.post("/webhook", async (request, response) => {
 
    if (bodyParam.object) {
        if (bodyParam.entry) {
-           for await (const changes of bodyParam.entry.changes) {
+           for (const changes of bodyParam.entry.changes) {
                if (changes.value.messages && changes.value.messages.length > 0) {
-                   for await (const message of changes.value.messages) {
+                   for (const message of changes.value.messages) {
                        const phoneNumberId = changes.value.metadata.phone_number_id;
                        const from = message.from;
                        const textMessage = message.text.body;
